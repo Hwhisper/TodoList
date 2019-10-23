bf835614e12836ac30d7e962fffc66b2ef6ee101
Hwhisper <50008886+Hwhisper@users.noreply.github.com>
2019-10-24 00:56:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';  // 引入bootstrap增强UI功能
 import Todolist from './Todolist';
 import './index.css';
 
-ReactDOM.render(
-  <Todolist />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Todolist />,document.getElementById('root'));
